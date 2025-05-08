0da640adf3904de945269a5e3772294133244d78
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2025-05-08 17:23:08 -0300

    Primera iteración de "Home" finalizada

---
+++ eLibreriaAlfaWeb/src/app/shared/components/title-and-description/title-and-description.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title-and-description',
+  templateUrl: './title-and-description.component.html',
+  styleUrls: ['./title-and-description.component.scss']
+})
+export class TitleAndDescriptionComponent {
+  @Input() title?: string;
+  @Input() description?: string;
+}
