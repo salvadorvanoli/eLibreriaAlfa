f2d1f627466a67ccbf47889a79c482cacdfdc767
joaqui-jz <joaquinjozamiseni@gmail.com>
2025-05-10 20:12:24 -0300

    Página de ver producto.

--- eLibreriaAlfaWeb/src/app/app.routes.ts
+++ eLibreriaAlfaWeb/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ViewProductComponent } from './pages/view-product/view-product.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: HomeComponent },
     { path: 'registro', component: RegisterComponent },
+    { path: 'product/:id', component: ViewProductComponent },
     { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // si querés que sea la ruta principal
     { path: '**', redirectTo: '/inicio' } // ruta por defecto
 ];
---
+++ eLibreriaAlfaWeb/src/app/pages/view-product/view-product.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Producto } from '../../models/producto';
+import { CarouselComponent } from '../../components/product/carousel/carousel.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { ModalComponent } from '../../components/product/modal/modal.component';
+
+@Component({
+  selector: 'app-view-product',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    CarouselComponent,
+    NavbarComponent,
+    FooterComponent,
+    ModalComponent 
+  ],
+  templateUrl: './view-product.component.html',
+  styleUrls: ['./view-product.component.scss']
+})
+
+export class ViewProductComponent implements OnInit {
+  producto?: Producto;
+  loading = true;
+  error?: string;
+  modalVisible = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = Number(params['id']);
+      if (!id || isNaN(id)) {
+        console.error('ID inválido:', params['id']);
+        this.router.navigate(['/']);
+        return;
+      }
+      this.productService.getById(id).subscribe({
+        next: (producto: Producto) => {
+          this.producto = producto;
+          this.loading = false;
+        },
+        error: error => {
+          console.error('Error obteniendo el producto:', error);
+          if (error.status === 404) {
+            console.log('Producto no encontrado, redirigiendo al home...');
+            this.router.navigate(['/']); // Redirige a la ruta raíz (home)
+          } else {
+            this.error = `Error al cargar el producto: ${error.message}`;
+            this.loading = false;
+          }
+        }
+      });
+    });  
+  }
+}
